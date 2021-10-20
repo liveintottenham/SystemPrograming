c5a86632b7783f743a3e5a858bec14b65dad16a0
min <immin.0113@gmail.com>
2021-10-20 01:37:55 -0700

    test 1

---
+++ system/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "math.h"
+
+int main() 
+{
+	printf("AD: %d \n",add(3,1));
+	printf("SUBTRAT: %d \n",subtract(3,1));
+	printf("MULTIPY: %d \n",multiply(3,1));
+	printf("DIVIE :%d \n", divide(3,1));
+}
